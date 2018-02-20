4eb576e0f8b7974fde84286f508a13d020b675ad
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-20 03:49:27 -0300

    [Technical Debt] Removendo imports não utilizados

--- src/main/java/br/com/caelum/mog/domain/repositories/ClienteRepository.java
+++ src/main/java/br/com/caelum/mog/domain/repositories/ClienteRepository.java
@@ -2,7 +2,6 @@ package br.com.caelum.mog.domain.repositories;
 
 import br.com.caelum.mog.domain.models.Cliente;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ClienteRepository {
