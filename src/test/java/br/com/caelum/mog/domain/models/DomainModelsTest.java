4138e6a418ed06afa38656f82c623639a95be8e9
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-20 03:04:29 -0300

    Removendo testes de interfaces do dominio, pois eles foram efetuados na camada de infra

--- src/test/java/br/com/caelum/mog/domain/models/DomainModelsTest.java
+++
@@ -1,9 +0,0 @@
-package br.com.caelum.mog.domain.models;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ClienteTest.class, CursoTest.class, PropostaTest.class})
-public interface DomainModelsTest {
-}
