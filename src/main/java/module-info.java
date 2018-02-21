14460524e648a2a76e14171da86a2620efa10e3a
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-21 17:32:06 -0300

    Removendo modularização

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module br.com.caelum.mog.domain {
-    requires hibernate.jpa;
-    requires spring.core;
-    exports br.com.caelum.mog.domain.models;
-    exports br.com.caelum.mog.domain.repositories;
-}
